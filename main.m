227ecdf92c313139f5f58cfde37942e0f2d6a9fe
Gerard Lacey <gerard.lacey@gmail.com>
2012-02-17 16:18:31 +0000

    first commit
    testing 123

---
+++ main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  Hello
+//
+//  Created by Gerard Lacey on 15/07/2010.
+//  Copyright SureWash 2010. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+	
+}
